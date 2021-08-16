73e3bcc292fce914566e127a9d6f0c9b26c2c853
Jessica Antunes <jessantunes@protonmail.com>
2021-08-16 12:54:29 -0400

    create comments to eplain what the code is doing

--- TwoNumberSum.js
+++ TwoNumberSum.js
@@ -1,13 +1,13 @@
 function twoNumberSum(array, targetSum) {
-    let numberHash = {};
+    let numberHash = {}; // created to keep numbers as keys in it
 
     for (const num of array) {
-        let diff = targetSum - num;
+        let diff = targetSum - num; // finds diff so we can see if that number can be found as a key in a hash
         if (numberHash[diff]) {
-            return [num, diff];
+            return [num, diff];  // returns the two numbers needed to get to target sum as an array
         } else {
-            numberHash[num] = true;
+            numberHash[num] = true;  // saves number with a true value (only the key matters)
         } 
     }
-    return [];     
+    return [];     // if no match returns empty array
 }
