f6306cfbc1b70f984c91176a7ad58e7c8b43a538
Jessica Antunes <jessantunes@protonmail.com>
2021-08-18 14:56:09 -0400

    add notes

--- NonConstructibleCoin.js
+++ NonConstructibleCoin.js
@@ -1,12 +1,12 @@
 function nonConstructibleChange(coins) {
-	let currentChange = 0;
-	coins.sort((a, b) => a - b);
+	let currentChange = 0; // total at the moment
+	coins.sort((a, b) => a - b); // sort coin values smallest to largest
 	
 	for (const coin of coins) {
-		if (coin > currentChange + 1) {
+		if (coin > currentChange + 1) { // if the next coin value is larger than the current value plus one then the amount of the CV + 1 is the answer
 			return currentChange + 1
 		}
-		currentChange += coin;
+		currentChange += coin; // if not add the coin to the current value
 	}
 		
   return currentChange + 1;
